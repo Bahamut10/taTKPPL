ca961b01779a3b5e003425b5aa1c6127b98a37e8
Morris Tokido <mor_riszzz@yahoo.com>
2016-05-25 23:44:45 +0700

    Kamera

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
+<<<<<<< Updated upstream
+=======
+  cube = require('bespoke-theme-cube'),
+  camera = require('bespoke-camera'),
+  voltaire = require('bespoke-theme-voltaire'),
+  nebula = require('bespoke-theme-nebula'),
+  build = require('bespoke-theme-build-wars'),
+>>>>>>> Stashed changes
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
@@ -22,9 +30,22 @@ bespoke.from('article', [
   backdrop(),
   scale(),
   hash(),
+  camera(),
   progress(),
   forms()
 ]);
+
+bespoke.horizontal.from('article', {
+  secondary: {
+      // Change the selector used to find notes, in a .bespoke-slide context 
+      notes: ".my-notes",
+      keys: {
+        // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent#Virtual_key_codes 
+        // Change the key used to toggle the secondary window 
+        toggle: 0x54, // (84) "T" key. 
+      }
+    }
+});
 
 // Prism syntax highlighting
 // This is actually loaded from "bower_components" thanks to
